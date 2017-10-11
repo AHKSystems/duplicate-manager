bc1061f655757eec1b831a364319a45993cfc7b0
Daniel Medrano <bestenglishking@hotmail.com>
2017-10-11 15:07:51 -0500

    Correccion de espaciado

--- dm.cpp
+++ dm.cpp
@@ -5,29 +5,29 @@ void escribir(std::string, const char*);
 
 int main(int argc, const char* argv[])
 {
-    int registros = 0, i, j;
-    std::string resultados = "pause\n\n";
+	int registros = 0, i, j;
+	std::string resultados = "pause\n\n";
 
 	std::string linea;
 	std::ifstream doc;
-    doc.open(argv[1], std::ios::in);
+	doc.open(argv[1], std::ios::in);
 
-    // Cuenta de registros
-    while(!doc.eof())
-    {
-        getline(doc, linea);
-        if (linea.length() > 0)
-            registros++;
-    }
+	// Cuenta de registros
+	while(!doc.eof())
+	{
+		getline(doc, linea);
+		if (linea.length() > 0)
+			registros++;
+	}
 
-    doc.close();
-    doc.open(argv[1], std::ios::in);
+	doc.close();
+	doc.open(argv[1], std::ios::in);
 
-    std::string segID[registros];
-    std::string rutas[registros];
+	std::string segID[registros];
+	std::string rutas[registros];
 
 	for(i = 0; i < registros; i++)
-    {
+	{
 		getline(doc, linea);
 		segID[i] = linea.substr(0, 19);
 		rutas[i] = linea.substr(20, std::string::npos);
@@ -37,22 +37,22 @@ int main(int argc, const char* argv[])
 	for(i = 1; i < registros; i++)
 		for(j = i - 1; j < i; j++)
 			if(segID[i] == segID[j])
-            {
-                resultados.append("del ");
+			{
+				resultados.append("del ");
 				resultados.append(rutas[i]);
 				resultados.append("\n");
-            }
+			}
 
-    escribir(resultados, argv[2]);
+	escribir(resultados, argv[2]);
 
 	return 1;
 }
 
 void escribir(std::string datos, const char* fileName)
 {
-    std::ofstream salida;
-    salida.open(fileName, std::ios::out);
+	std::ofstream salida;
+	salida.open(fileName, std::ios::out);
 
-    salida << datos;
-    salida.close();
+	salida << datos;
+	salida.close();
 }

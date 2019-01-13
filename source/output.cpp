9a5561da7c1526773920b7ca53f8843a62676168
Daniel Medrano <bestenglishking@hotmail.com>
2019-01-13 10:49:54 -0600

    Prueba

--- source/output.cpp
+++ source/output.cpp
@@ -1,134 +1,57 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "date.h"
 #include "fs.h"
 #include "output.h"
 
+using std::endl;
 using std::ifstream;
-using std::ofstream;
 using std::ios;
+using std::ofstream;
 using std::string;
-
-unsigned int get_file_numlines(string& filename)
-{
-    ifstream ifs(filename, ios::in);
-    unsigned int reg_count = 0;
-
-    while (!ifs.eof())
-    {
-        string buffer;
-        getline(ifs, buffer);
-        if (buffer.length() > 0)
-            reg_count++;
-    }
-    ifs.close();
-
-    return reg_count;
-}
-
-int remove_files(string input_file, string output_file, bool generate_log)
-{
-    int regCount;
-    bool writeOut;
-    ifstream iStream;
-    ofstream oStream;
-
-    const char* MSG_DELETED = "  Eliminado    -> ";
-    const char* MSG_NOT_DELETED = "  No eliminado -> ";
-
-    // Revisa si existe el directorio de salida
-    if (generate_log && directory_exists(output_file))
-        writeOut = true;
-    else if (generate_log && !directory_exists(output_file))
-        throw 15;
-    else
-        writeOut = false;
-
-    regCount = get_file_numlines(input_file);
-
-    string segID[regCount];
-    string paths[regCount];
-
-    iStream.open(input_file, ios::in);
-
-    for (int i = 0; i < regCount; i++)
-    {
-        string buffer;
-        getline(iStream, buffer);
-        segID[i] = buffer.substr(0, 19);
-        paths[i] = buffer.substr(20, string::npos);
-    }
-    iStream.close();
-
-    if (writeOut)
-        oStream.open(output_file, ios::app);
-
-    for (int i = 1; i < regCount; i++)
-        for (int j = i - 1; j < i; j++)
-            if (segID[i] == segID[j])
-            {
-                string bufferLog = get_datetime();
-
-                if (remove(paths[i].c_str()) == 0)
-                    bufferLog.append(MSG_DELETED);
-                else
-                    bufferLog.append(MSG_NOT_DELETED);
-
-                bufferLog.append(paths[i]);
-                bufferLog.append("\n");
-
-                if (writeOut)
-                    oStream << bufferLog;
-            }
-
-    if (writeOut)
-        oStream.close();
-
-    return 1;
-}
+using std::vector;
 
 int write_batch_file(string input_file, string output_file)
 {
-    int regCount;
-    ifstream iStream;
-    ofstream oStream;
-
     // Revisa si existe el directorio de salida
     if (!directory_exists(output_file))
         throw 15;
 
-    regCount = get_file_numlines(input_file);
+    vector<string> paths;
 
-    string segID[regCount];
-    string paths[regCount];
+    ifstream ifs(input_file, ios::in);
+    if (!ifs.is_open())
+        return 10;
 
-    iStream.open(input_file, ios::in);
-
-    for (int i = 0; i < regCount; i++)
+    while (ifs.good())
     {
         string buffer;
-        getline(iStream, buffer);
-        segID[i] = buffer.substr(0, 19);
-        paths[i] = buffer.substr(20, string::npos);
+        getline(ifs, buffer);
+
+        paths.push_back(buffer.substr(20, string::npos));
     }
-    iStream.close();
+    ifs.close();
 
-    oStream.open(output_file, ios::app);
-    oStream << "pause\n" << endl;
+    ofstream ofs(output_file, ios::app);
+    if (!ofs.is_open())
+        return 11;
 
-    for (int i = 1; i < regCount; i++)
+    ofs << "pause" << endl << endl;
+
+    for (int i = 1; i < paths.size(); i++)
         for (int j = i - 1; j < i; j++)
             if (segID[i] == segID[j])
             {
                 string line;
-                line = "del \"";
+                line.assign("del \"");
                 line.append(paths[i]);
                 line.append("\"");
 
-                oStream << line << endl;
+                ofs << line << endl;
             }
-    oStream.close();
+    ofs.close();
 
-    return 1;
+    return 0;
 }

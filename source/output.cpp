59a1615a7cc65704b19ff740d33bcec9125dfa8c
Daniel Medrano <bestenglishking@hotmail.com>
2019-01-13 11:51:25 -0600

    Corregir cositas

--- source/output.cpp
+++ source/output.cpp
@@ -13,6 +13,55 @@ using std::ofstream;
 using std::string;
 using std::vector;
 
+int get_lines(string, vector<string>&, vector<string>&);
+
+int remove_files(string input_file, string output_file, bool generate_log)
+{
+    bool writeOut;
+
+    // Revisa si existe el directorio de salida
+    if (generate_log && directory_exists(output_file))
+        writeOut = true;
+    else if (generate_log && !directory_exists(output_file))
+        throw 15;
+    else
+        writeOut = false;
+
+    ofstream ofs;
+
+    vector<string> segment_ids;
+    vector<string> paths;
+
+    const char* MSG_DELETED = "  Eliminado    -> ";
+    const char* MSG_NOT_DELETED = "  No eliminado -> ";
+
+    if (writeOut)
+        oStream.open(output_file, ios::app);
+
+    for (int i = 1; i < regCount; i++)
+        for (int j = i - 1; j < i; j++)
+            if (segment_ids[i] == segment_ids[j])
+            {
+                string bufferLog = get_datetime();
+
+                if (remove(paths[i].c_str()) == 0)
+                    bufferLog.append(MSG_DELETED);
+                else
+                    bufferLog.append(MSG_NOT_DELETED);
+
+                bufferLog.append(paths[i]);
+                bufferLog.append("\n");
+
+                if (writeOut)
+                    oStream << bufferLog;
+            }
+
+    if (writeOut)
+        oStream.close();
+
+    return 0;
+}
+
 int write_batch_file(string input_file, string output_file)
 {
     // Revisa si existe el directorio de salida
@@ -22,7 +71,36 @@ int write_batch_file(string input_file, string output_file)
     vector<string> segment_ids;
     vector<string> paths;
 
-    ifstream ifs(input_file, ios::in);
+    int read_result = get_lines(input_file, paths, segment_ids);
+
+    if (read_result == 0)
+    {
+        ofstream ofs(output_file, ios::app);
+        if (!ofs.is_open())
+            return 11;
+
+        ofs << "pause" << endl << endl;
+
+        for (int i = 1; i < paths.size(); i++)
+            for (int j = i - 1; j < i; j++)
+                if (segment_ids[i] == segment_ids[j])
+                {
+                    string line;
+                    line.assign("del \"");
+                    line.append(paths[i]);
+                    line.append("\"");
+
+                    ofs << line << endl;
+                }
+        ofs.close();
+    }
+
+    return read_result;
+}
+
+int get_lines(string filename, vector<string>& paths, vector<string>& segments)
+{
+    ifstream ifs(filename, ios::in);
     if (!ifs.is_open())
         return 10;
 
@@ -31,29 +109,10 @@ int write_batch_file(string input_file, string output_file)
         string buffer;
         getline(ifs, buffer);
 
-        segment_ids.push_back(buffer.substr(0, 19));
+        segments.push_back(buffer.substr(0, 19));
         paths.push_back(buffer.substr(20, string::npos));
     }
     ifs.close();
-
-    ofstream ofs(output_file, ios::app);
-    if (!ofs.is_open())
-        return 11;
-
-    ofs << "pause" << endl << endl;
-
-    for (int i = 1; i < paths.size(); i++)
-        for (int j = i - 1; j < i; j++)
-            if (segment_ids[i] == segment_ids[j])
-            {
-                string line;
-                line.assign("del \"");
-                line.append(paths[i]);
-                line.append("\"");
-
-                ofs << line << endl;
-            }
-    ofs.close();
 
     return 0;
 }
